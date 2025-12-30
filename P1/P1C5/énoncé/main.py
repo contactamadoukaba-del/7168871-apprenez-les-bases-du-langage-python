91c10fb608b7d06d4170f71349fedcf1fac0fd20
contact22 <contact.amadoukaba@gmail.com>
2025-12-30 04:14:05 +0100

    Update main.py
    moi c'ets kaba amadou et c'est le rendu de mon exerci

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,22 @@
+nom = "kaba amadou"
+age = 22
+taille = 25.3
+est_etudiant = True
+
+kaba amadou
+ print(nom , age)
+kaba amadou 22
+ print (nom, age, taille, est_etudiant)
+kaba amadou 22 25.3 True
+
+
+ type(nom)
+<class 'str'>
+ type(age)
+<class 'int'>
+ type(taille)
+<class 'float'>
+ type(est_etudiant)
+<class 'bool'>
+ 
 # Écrivez votre code ici !
